5a3488bb73f3ceb4575591626debf8e2cc8a0b4a
noumecha spaker <noumechaivan@gmail.com>
2023-08-01 05:50:59 +0100

    adding some twig dependencies with composer

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -4,15 +4,27 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MoviesController extends AbstractController
 {
-    #[Route('/movies', name: 'app_movies')]
-    public function index(): JsonResponse
+    #[Route('/movies', name: 'movies')]
+    public function index(): Response
     {
+        return $this->render("index.html.twig", [
+            "title" => "World of Narnia : chapter 2",
+        ]);
+    }
+
+    /**
+     * testMethod
+     *
+     * @Route("/test", name="test")
+     */
+    public function testMethod(): JsonResponse {
         return $this->json([
-            'message' => 'Welcome to your new controller!',
+            'message' => 'test method',
             'path' => 'src/Controller/MoviesController.php',
         ]);
     }
